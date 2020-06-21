197b98075b11a4fc9a8b0ef56c914be6680aa3dc
webmaster-pnn <machadopnn@gmail.com>
2020-06-21 00:54:37 -0300

    Paginação com parametros opcionais

--- src/main/java/com/carlos/cursomc/services/CategoriaService.java
+++ src/main/java/com/carlos/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.carlos.cursomc.domain.Categoria;
@@ -58,4 +61,22 @@ public class CategoriaService {
 		}
 		
 	}
+	
+//	PAGINAÇÃO
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
+				orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
