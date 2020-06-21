4aae365c70c706a4e504ce3e38a50e1564d8b0df
webmaster-pnn <machadopnn@gmail.com>
2020-06-21 15:06:32 -0300

    CategoriaService e construtor de pagamento

--- src/main/java/com/carlos/cursomc/services/CategoriaService.java
+++ src/main/java/com/carlos/cursomc/services/CategoriaService.java
@@ -47,8 +47,9 @@ public class CategoriaService {
 	
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	
@@ -80,7 +81,12 @@ public class CategoriaService {
 	
 	
 	
-	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+		
+		
+		
+	}
 	
 	
 	
