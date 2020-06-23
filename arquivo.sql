ca554be49ad08d148debad262ac66621c38270a3
webmaster-pnn <machadopnn@gmail.com>
2020-06-23 18:26:50 -0300

    adicionando plugins e arquivo system.properties

---
+++ arquivo.sql
@@ -0,0 +1,531 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 23-Jun-2020 às 22:11
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `curso_spring`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE if not exists `categoria` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nome`) VALUES
+(1, 'Informatica'),
+(2, 'Escritorio'),
+(3, 'Cama, mesa e Banho'),
+(4, 'Eletronicos'),
+(5, 'Jardinagem'),
+(6, 'Decoração'),
+(7, 'Perfumaria');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cidade`
+--
+
+CREATE TABLE `cidade` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `estado_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `cidade`
+--
+
+INSERT INTO `cidade` (`id`, `nome`, `estado_id`) VALUES
+(1, 'Uberlândia', 1),
+(2, 'São Paulo', 2),
+(3, 'Campinas', 2),
+(4, 'Niterói', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id` int(11) NOT NULL,
+  `cpf_ou_cnpj` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `tipo` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`id`, `cpf_ou_cnpj`, `email`, `nome`, `tipo`) VALUES
+(1, '54545454', 'maria@gmail.com', 'Maria Silva', 1),
+(2, '17131346756', 'machadopnn@gmail.com', 'Carlos henrique ', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `endereco`
+--
+
+CREATE TABLE `endereco` (
+  `id` int(11) NOT NULL,
+  `bairro` varchar(255) DEFAULT NULL,
+  `cep` varchar(255) DEFAULT NULL,
+  `complemento` varchar(255) DEFAULT NULL,
+  `logradouro` varchar(255) DEFAULT NULL,
+  `numero` varchar(255) DEFAULT NULL,
+  `cidade_id` int(11) DEFAULT NULL,
+  `cliente_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `endereco`
+--
+
+INSERT INTO `endereco` (`id`, `bairro`, `cep`, `complemento`, `logradouro`, `numero`, `cidade_id`, `cliente_id`) VALUES
+(1, 'Jardim', '38922250', 'Apto 203', 'Rua Flores', '300', 1, 1),
+(2, 'Centro', '58944250', 'Sala 800', 'Avenida Mattos', '105', 2, 1),
+(3, 'Barreto', '24110360', 'casa 2', 'Rua Francisco Esteves', '69', 4, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `estado`
+--
+
+CREATE TABLE `estado` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `estado`
+--
+
+INSERT INTO `estado` (`id`, `nome`) VALUES
+(1, 'Minas Gerais'),
+(2, 'São Paulo'),
+(3, 'Rio de Janeiro');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `item_pedido`
+--
+
+CREATE TABLE `item_pedido` (
+  `desconto` double DEFAULT NULL,
+  `preco` double DEFAULT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `pedido_id` int(11) NOT NULL,
+  `produto_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `item_pedido`
+--
+
+INSERT INTO `item_pedido` (`desconto`, `preco`, `quantidade`, `pedido_id`, `produto_id`) VALUES
+(0, 2000, 1, 1, 1),
+(0, 200, 2, 1, 3),
+(100, 800, 1, 2, 2),
+(0, 1200.5, 1, 3, 1),
+(0, 20.8, 2, 3, 3),
+(0, 1200.5, 1, 4, 1),
+(0, 20.8, 2, 4, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pagamento`
+--
+
+CREATE TABLE `pagamento` (
+  `pedido_id` int(11) NOT NULL,
+  `estado_pagamento` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `pagamento`
+--
+
+INSERT INTO `pagamento` (`pedido_id`, `estado_pagamento`) VALUES
+(1, 2),
+(2, 1),
+(3, 1),
+(4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pagamento_com_boleto`
+--
+
+CREATE TABLE `pagamento_com_boleto` (
+  `data_pagamento` datetime DEFAULT NULL,
+  `data_vencimento` datetime DEFAULT NULL,
+  `pedido_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `pagamento_com_boleto`
+--
+
+INSERT INTO `pagamento_com_boleto` (`data_pagamento`, `data_vencimento`, `pedido_id`) VALUES
+(NULL, '2017-10-20 02:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pagamento_com_cartao`
+--
+
+CREATE TABLE `pagamento_com_cartao` (
+  `numero_parcelas` int(11) DEFAULT NULL,
+  `pedido_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `pagamento_com_cartao`
+--
+
+INSERT INTO `pagamento_com_cartao` (`numero_parcelas`, `pedido_id`) VALUES
+(6, 1),
+(10, 3),
+(10, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id` int(11) NOT NULL,
+  `instante` datetime DEFAULT NULL,
+  `cliente_id` int(11) DEFAULT NULL,
+  `endereco_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `pedido`
+--
+
+INSERT INTO `pedido` (`id`, `instante`, `cliente_id`, `endereco_id`) VALUES
+(1, '2017-09-30 13:32:00', 1, 1),
+(2, '2017-10-10 22:35:00', 1, 2),
+(3, '2020-06-22 08:02:03', 1, NULL),
+(4, '2020-06-22 08:02:06', 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `preco` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `nome`, `preco`) VALUES
+(1, 'Computador', 1200.5),
+(2, 'Impressora', 835.5),
+(3, 'Mouse', 20.8),
+(4, 'Mesa de escritorio', 300.8),
+(5, 'Toalha', 20.8),
+(6, 'Colcha', 20.8),
+(7, 'Tv', 20.8),
+(8, 'Roçadeira', 20.8),
+(9, 'Abajour', 20.8),
+(10, 'Pendente', 20.8),
+(11, 'Shampoo', 20.8);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produto_categoria`
+--
+
+CREATE TABLE `produto_categoria` (
+  `produto_id` int(11) NOT NULL,
+  `categoria_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `produto_categoria`
+--
+
+INSERT INTO `produto_categoria` (`produto_id`, `categoria_id`) VALUES
+(1, 1),
+(1, 4),
+(2, 1),
+(2, 2),
+(2, 4),
+(3, 1),
+(3, 4),
+(4, 2),
+(5, 3),
+(6, 3),
+(7, 4),
+(8, 5),
+(9, 6),
+(10, 6),
+(11, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `telefone`
+--
+
+CREATE TABLE `telefone` (
+  `cliente_id` int(11) NOT NULL,
+  `telefones` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `telefone`
+--
+
+INSERT INTO `telefone` (`cliente_id`, `telefones`) VALUES
+(1, '2511115544'),
+(1, '11225544'),
+(2, '21982997212'),
+(2, '25114454545');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `cidade`
+--
+ALTER TABLE `cidade`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FKkworrwk40xj58kevvh3evi500` (`estado_id`);
+
+--
+-- Índices para tabela `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UK_cmxo70m08n43599l3h0h07cc6` (`email`);
+
+--
+-- Índices para tabela `endereco`
+--
+ALTER TABLE `endereco`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK8b1kcb3wucapb8dejshyn5fsx` (`cidade_id`),
+  ADD KEY `FK8s7ivtl4foyhrfam9xqom73n9` (`cliente_id`);
+
+--
+-- Índices para tabela `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `item_pedido`
+--
+ALTER TABLE `item_pedido`
+  ADD PRIMARY KEY (`pedido_id`,`produto_id`),
+  ADD KEY `FKtk55mn6d6bvl5h0no5uagi3sf` (`produto_id`);
+
+--
+-- Índices para tabela `pagamento`
+--
+ALTER TABLE `pagamento`
+  ADD PRIMARY KEY (`pedido_id`);
+
+--
+-- Índices para tabela `pagamento_com_boleto`
+--
+ALTER TABLE `pagamento_com_boleto`
+  ADD PRIMARY KEY (`pedido_id`);
+
+--
+-- Índices para tabela `pagamento_com_cartao`
+--
+ALTER TABLE `pagamento_com_cartao`
+  ADD PRIMARY KEY (`pedido_id`);
+
+--
+-- Índices para tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK30s8j2ktpay6of18lbyqn3632` (`cliente_id`),
+  ADD KEY `FKsot2og0lvhm2t4kuu9s2obef3` (`endereco_id`);
+
+--
+-- Índices para tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `produto_categoria`
+--
+ALTER TABLE `produto_categoria`
+  ADD KEY `FKq3g33tp7xk2juh53fbw6y4y57` (`categoria_id`),
+  ADD KEY `FK1c0y58d3n6x3m6euv2j3h64vt` (`produto_id`);
+
+--
+-- Índices para tabela `telefone`
+--
+ALTER TABLE `telefone`
+  ADD KEY `FK8aafha0njkoyoe3kvrwsy3g8u` (`cliente_id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de tabela `cidade`
+--
+ALTER TABLE `cidade`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `endereco`
+--
+ALTER TABLE `endereco`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de tabela `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `cidade`
+--
+ALTER TABLE `cidade`
+  ADD CONSTRAINT `FKkworrwk40xj58kevvh3evi500` FOREIGN KEY (`estado_id`) REFERENCES `estado` (`id`);
+
+--
+-- Limitadores para a tabela `endereco`
+--
+ALTER TABLE `endereco`
+  ADD CONSTRAINT `FK8b1kcb3wucapb8dejshyn5fsx` FOREIGN KEY (`cidade_id`) REFERENCES `cidade` (`id`),
+  ADD CONSTRAINT `FK8s7ivtl4foyhrfam9xqom73n9` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`);
+
+--
+-- Limitadores para a tabela `item_pedido`
+--
+ALTER TABLE `item_pedido`
+  ADD CONSTRAINT `FK60ym08cfoysa17wrn1swyiuda` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`),
+  ADD CONSTRAINT `FKtk55mn6d6bvl5h0no5uagi3sf` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`);
+
+--
+-- Limitadores para a tabela `pagamento`
+--
+ALTER TABLE `pagamento`
+  ADD CONSTRAINT `FKthad9tkw4188hb3qo1lm5ueb0` FOREIGN KEY (`pedido_id`) REFERENCES `pedido` (`id`);
+
+--
+-- Limitadores para a tabela `pagamento_com_boleto`
+--
+ALTER TABLE `pagamento_com_boleto`
+  ADD CONSTRAINT `FKcr74vrxf8nfph0knq2bho8doo` FOREIGN KEY (`pedido_id`) REFERENCES `pagamento` (`pedido_id`);
+
+--
+-- Limitadores para a tabela `pagamento_com_cartao`
+--
+ALTER TABLE `pagamento_com_cartao`
+  ADD CONSTRAINT `FKta3cdnuuxclwfh52t4qi432ow` FOREIGN KEY (`pedido_id`) REFERENCES `pagamento` (`pedido_id`);
+
+--
+-- Limitadores para a tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `FK30s8j2ktpay6of18lbyqn3632` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
+  ADD CONSTRAINT `FKsot2og0lvhm2t4kuu9s2obef3` FOREIGN KEY (`endereco_id`) REFERENCES `endereco` (`id`);
+
+--
+-- Limitadores para a tabela `produto_categoria`
+--
+ALTER TABLE `produto_categoria`
+  ADD CONSTRAINT `FK1c0y58d3n6x3m6euv2j3h64vt` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`),
+  ADD CONSTRAINT `FKq3g33tp7xk2juh53fbw6y4y57` FOREIGN KEY (`categoria_id`) REFERENCES `categoria` (`id`);
+
+--
+-- Limitadores para a tabela `telefone`
+--
+ALTER TABLE `telefone`
+  ADD CONSTRAINT `FK8aafha0njkoyoe3kvrwsy3g8u` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
