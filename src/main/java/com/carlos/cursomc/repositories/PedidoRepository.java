9ab8aee8b692776ec26b8edb0ed52f019fad2225
webmaster-pnn <machadopnn@gmail.com>
2020-06-25 14:18:20 -0300

    Restrição de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/carlos/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/carlos/cursomc/repositories/PedidoRepository.java
@@ -1,12 +1,18 @@
 package com.carlos.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.carlos.cursomc.domain.Cliente;
 import com.carlos.cursomc.domain.Pedido;
 
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
 
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
